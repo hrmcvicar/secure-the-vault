56da707c8354c395b139489d0fd09f2841ba6d7f
Hayley McVicar <hrmcvicar@gmail.com>
2025-10-26 11:44:31 -0700

    cleaned up code and console log

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 let code1;
 
-const num1 = 7;
+/*const num1 = 7;
 const num2 = 3;
-code1 = num1 + num2;
-console.log(code1);
+code1 = num1 + num2;*/
+code1 = 7 + 3;
+//console.log(code1);
 
-const num3 = 130;
+/*const num3 = 130;
 const num4 = 10;
-const code2 = (num3 - num4) / num2;
-console.log(code2);
+const code2 = (num3 - num4) / num2;*/
+const code2 = (130 - 10) / 3;
+//console.log(code2);
 
-const num5 = 35;
-const code3 = (num3 % num1) + num5;
-console.log(code3);
+/*const num5 = 35;
+const code3 = (num3 % num1) + num5;*/
+const code3 = (130 % 7) + 35;
+//console.log(code3);
 
-const message = "The vault has been secured, The combination is:";
-console.log(message);
+const message = "The vault has been secured. The combination is:";
+//console.log(message);
 
 const codeA = code1 + "-" + code2 + "-" + code3;
-console.log(codeA);
+//console.log(codeA);
 
 const codeB = `${code1}-${code2}-${code3}`;
-console.log(codeB);
+//console.log(codeB);
 
-console.log(message);
+/*console.log(message);
 console.log(codeA);
-console.log(codeB);
+console.log(codeB);*/
+console.log(`${message} ${codeB}`);
